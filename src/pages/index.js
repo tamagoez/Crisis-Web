2fb91e8132b2bdfc80621a61a8e4f5b496036d2b
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-06-19 14:07:55 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,12 +13,12 @@ export default function Home() {
 
       <div className="hero min-h-screen bg-gradient-to-br from-gray-600 to-red-300">
         <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-center text-white">
+        <div className="hero-content text-center text-white/95">
           <div className="max-w-md">
             <h1 className="mb-5 text-6xl font-bold">Crisis</h1>
             <p className="mb-5 text-sm">
               RPGホラーゲーム
-              <div className="divider text-white" />
+              <div className="divider divide-slate-400/25" />
               リアは中学一年生の2週間目を過ごそうとしていた。
               <br />
               しかし彼女はこの世の不条理から逃れるために自殺を図った。
